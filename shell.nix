35f1329ad513707804c07ade43b4e9db73b1440a
anngo-1 <andngo2004@gmail.com>
2025-02-21 20:47:21 -0800

    update yfinance version in pip

--- shell.nix
+++ shell.nix
@@ -1,33 +1,35 @@
 { pkgs ? import <nixpkgs> {} }:
-
 pkgs.mkShell {
   buildInputs = [
     pkgs.gnumake
     pkgs.gcc
     pkgs.curl
     pkgs.python3
-    pkgs.python3Packages.yfinance
+    pkgs.python3Packages.pandas
     pkgs.python3Packages.pip
     pkgs.nodejs  
   ];
-
   nativeBuildInputs = [
     pkgs.pkg-config
   ];
-
   shellHook = ''
     export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:./"
     export LIBRARY_PATH="$LIBRARY_PATH:${pkgs.curl}/lib"
-
     export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${pkgs.curl}/lib/pkgconfig"
-    echo "Development shell for advanced_daytrading_bot_api activated!"
+    
+    # Create and activate a virtual environment
+    python -m venv .venv
+    source .venv/bin/activate
+    
+    # Install latest yfinance
+    pip install --upgrade yfinance
+    
+    echo "Development shell for activated!"
+    echo "Python virtual environment created and activated"
+    echo "Latest yfinance installed"
     echo "C++ compiler (g++) and make are available."
-    echo "httplib.h and nlohmann_json.hpp should be placed in the project root directory."
     echo "Run 'make' to compile the C++ API."
-
     echo ""
     echo "Node.js and npm are also available for frontend development."
-    echo "You can use 'npm install' to install Node.js dependencies."
-    echo "Then use 'npm run dev' to start your development server (if configured in package.json)."
   '';
 }

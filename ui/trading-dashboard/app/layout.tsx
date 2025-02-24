a5a744e4ea008e19ebd38af5f86d1af209f2c28a
anngo-1 <andngo2004@gmail.com>
2025-02-24 13:37:38 -0800

    force light theme

--- ui/trading-dashboard/app/layout.tsx
+++ ui/trading-dashboard/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-mantine-color-scheme="light">
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
